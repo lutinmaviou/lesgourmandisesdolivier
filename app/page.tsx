93316f3ffff18ddcf0d2298ab23c4d0ac7e7e5e4
Bertrand Bourion <bertrandbou@hotmail.fr>
2023-08-07 16:01:50 +0200

    ready for deployement

--- app/page.tsx
+++ app/page.tsx
@@ -1,85 +1,39 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import './page.css';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="container">
+      <div
+        className="grid background"
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fit, minmax(100%, auto))',
+        }}
+      >
+        <div className="back">
+          <Image src="/fond.png" alt="Image de fond" fill sizes="100vw" />
         </div>
+        <h1 className="agency main-title">
+          Les Gourmandises d'<span className="red-letter">O</span>livier
+        </h1>
+        {/* <h4 className="contact">
+          <span className="underline">Mobile</span>: 06 28 02 38 16 /{' '}
+          <span className="underline">email</span> : lesgourmandisesdolivier@gmail.com{' '}
+        </h4> */}
       </div>
-
-      <div className={styles.center}>
-        <h1>Les Gourmandises d'Olivier</h1>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <main className="main">
+        <h2 className="h2">Site en cours de réalisation</h2>
+        <h3 className="h3">A très bientôt !</h3>
+        <div className="gif">
+          <Image
+            src="/gif-en-construction.gif"
+            alt="construction"
+            width={200}
+            height={200}
+          />
+        </div>
+      </main>
+    </div>
+  );
 }
